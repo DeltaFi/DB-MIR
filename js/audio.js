545dd48eaf350283c5c877d7553c210c91b057bb
Dragos Rotaru <info@dragosrotaru.com>
2016-06-18 02:02:49 -0400

    on its way..

--- js/audio.js
+++ js/audio.js
@@ -10,7 +10,7 @@ var source = audioCtx.createMediaElementSource(audio);
 var gainNode = audioCtx.createGain();
 var analyser = audioCtx.createAnalyser();
 var threshold = 0;
-var decay = 0.998;
+var decay = 0.999;
 
 analyser.fftSize = 2048;
 analyser.smoothingTimeConstant = 0.1;
@@ -22,81 +22,30 @@ source.connect(analyser);
 analyser.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 
-//number of images in each category, respectively.
-var nImages = 82;
-var nTops = 97;
-var nbackgrounds = 175;
-
-backgroundsrc = [];
-//extended range for foreground gifs
-
-var extendedNImages = Math.floor(nImages * nothingnessFactor);
-var extendedNTops = Math.floor(nTops * nothingnessFactor);
+//number of images
+var nbackgrounds = 156;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
 function updateBackground() {
-if (backgroundsrc.length < 5) {
-    for (var i = 0; i < 25; i++) {
-        var background = getRandomInt(nbackgrounds);
-        backgroundsrc.push("url(backgrounds/background" + background.toString() + ".gif)");
-        $('#preload').css("background-image", "url(backgrounds/background" + background.toString() + ".gif)");
-    }
-    $('#background').css("background-image", backgroundsrc.shift());
+  var background = getRandomInt(nbackgrounds);
+  $('#background').css("background-image", "url(backgrounds/background" + background.toString() + ".gif)");
 
-} else {
-    $('#background').css("background-image", backgroundsrc.shift());
-}
-}
-
-function updateTop() {
-  var top = getRandomInt(extendedNTops);
-  if (top <= nTops) {
-    $('#top').css("background-image", "url(top/image" + top.toString() + ".gif)");
-  }
-}
-
-function updateLeft() {
-  var left = getRandomInt(extendedNImages);
-  if (left <= nImages) {
-    $('#left').css("background-image", "url(images/image" + left.toString() + ".gif)");
-  }
-}
-
-function updateRight() {
-  var right = getRandomInt(extendedNImages);
-  if (right <= nImages) {
-    $('#right').css("background-image", "url(images/image" + right.toString() + ".gif)");
-  }
 }
 
 function draw() {
   analyser.getByteFrequencyData(frequencyData);
-  var bass = 0;
+  var freqSum = 0;
   for (var i = 0; i < frequencyBinCount; i++) {
-    bass = bass + frequencyData[i];
+    freqSum = freqSum + frequencyData[i];
   }
-  var bassmean = bass / frequencyBinCount;
-  if (bassmean * bassmean > threshold) {
-    preloadStop = true;
-    threshold = bassmean * bassmean;
+  var freqMean = freqSum / frequencyBinCount;
+  if (freqMean * freqMean > threshold) {
+    threshold = freqMean * freqMean;
     updateBackground();
-    //updateRight();
-    //updateTop();
-    //updateLeft();
   } else {
     threshold = threshold * decay;
   }
 }
-
-function preload(num){
-    for (var i = 0; i < num; i++) {
-        var background = getRandomInt(nbackgrounds);
-        backgroundsrc.push("url(backgrounds/background" + background.toString() + ".gif)");
-        $('#preload').css("background-image", "url(backgrounds/background" + background.toString() + ".gif)");
-    }
-}
-
-preload(175);
