cba2cbafff12809bd168089aa22bd45ce4e8dcbe
Dragos Rotaru <info@dragosrotaru.com>
2016-06-22 20:09:01 -0400

    play features

--- js/audio.js
+++ js/audio.js
@@ -10,8 +10,8 @@ audio.crossOrigin = "anonymous";
 var source = audioCtx.createMediaElementSource(audio);
 var gainNode = audioCtx.createGain();
 var analyser = audioCtx.createAnalyser();
-var threshold = 0;
-var decay = 0.9999;
+var threshold = 1;
+var decay = 0.9993;
 
 var subSum = 0;
 var bassSum = 0;
