9e343d287c55df396f949534b83c2344c58520a2
Dragos Rotaru <info@dragosrotaru.com>
2016-06-22 20:19:24 -0400

    play features

--- js/audio.js
+++ js/audio.js
@@ -78,7 +78,7 @@ function draw() {
   var freqmidMean = midSum / 93;
   var freqtrebMean = trebSum / 412;
   var freqweightedMean = freqsubMean + 2 * freqbassMean + freqmidMean + freqtrebMean;
-  if (freqweightedMean > threshold) {
+  if (0.5 * freqweightedMean * freqweightedMean  > threshold) {
     threshold = freqweightedMean * freqweightedMean;
     display();
     loadImage(false);
